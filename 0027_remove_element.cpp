1bb5b8257d237cbf5aa9a0b6a693a76cf72fc6ff
3coco <cocostrong1@outlook.com>
2018-02-16 02:58:07 -0800

    added 29

---
+++ 0027_remove_element.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution27 {
+public:
+    int removeElement(vector<int> &nums, int val) {
+       int result = 0;
+       for(int i = 0; i < nums.size(); i++) {
+           if(nums[i] != val) {
+               nums[result++] = nums[i];
+           }
+       }
+
+       return result;
+    }
+};
+
+int main() {
+    Solution27 sol;
+    int nums[4] = {3, 2, 2, 3};
+    vector<int> vec1(nums, nums + sizeof(nums) / sizeof(nums[0]));
+    int ret = sol.removeElement(vec1, 3);
+    int ans = 2;
+    if(ret != ans) {
+        cout << "Test#1 failed" << endl;
+    }
+}
